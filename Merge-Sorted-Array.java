fe4ee7d7dc40742d71636428dbd53f28b4ff110a
suryadwd <surajdwivedi644@gmail.com>
2025-11-15 12:44:42 +0530

    first commit

---
+++ Merge-Sorted-Array.java
@@ -0,0 +1,20 @@
+// 88 Merge Sorted Array
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int res[] = new int[m+n];
+        int i = 0,
+            j = 0,
+            k = 0;
+        while(i < m && j < n){
+            if(nums1[i] < nums2[j]){
+                res[k++] = nums1[i++];
+            }else res[k++] = nums2[j++];
+        }
+        while(i < m) res[k++] = nums1[i++];
+        while(j < n) res[k++] = nums2[j++];
+        for(int x = 0; x < m+n; x++){
+            nums1[x] = res[x];
+        }
+    }
+}
